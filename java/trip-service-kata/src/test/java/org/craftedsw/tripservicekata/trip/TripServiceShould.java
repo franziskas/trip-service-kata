9a86f0a959176935ff7eeaa4bdab420fca040565
Franziska Sauerwein <franzi@codurance.com>
2015-07-09 16:51:43 +0100

    added test for user who is not friends with logged in user

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceShould.java
@@ -1,10 +1,15 @@
 package org.craftedsw.tripservicekata.trip;
 
+import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class TripServiceShould {
     @Rule
@@ -23,5 +28,27 @@ public class TripServiceShould {
         thrown.expect(UserNotLoggedInException.class);
 
         tripService.getTripsByUser(null);
+    }
+
+    @Test
+    public void
+    show_no_trips_if_logged_in_user_is_not_friends_with_user() {
+        User bob = new User();
+        final User alice = new User();
+        TripService tripService = new TripService() {
+            @Override
+            protected User getLoggedInUser() {
+                return alice;
+            }
+
+            @Override
+            protected List<Trip> getTripsBy(User user) {
+                return asList(new Trip(), new Trip());
+            }
+        };
+
+        List<Trip> trips = tripService.getTripsByUser(bob);
+
+        assertThat(trips.size(), is(0));
     }
 }
