22bb461943f79fb2d2206dd0677f86300cec8992
Franziska Sauerwein <franzi@codurance.com>
2015-07-09 17:15:34 +0100

    refactoring: simplified code to show intent

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,31 +1,37 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
 
+import static java.util.Collections.emptyList;
+
 public class TripService {
 
     public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-        List<Trip> tripList = new ArrayList<Trip>();
         User loggedUser = getLoggedInUser();
-        boolean isFriend = false;
-        if (loggedUser != null) {
-            for (User friend : user.getFriends()) {
-                if (friend.equals(loggedUser)) {
-                    isFriend = true;
-                    break;
-                }
-            }
-            if (isFriend) {
-                tripList = getTripsBy(user);
-            }
-            return tripList;
-        } else {
+        if (loggedUser == null) {
             throw new UserNotLoggedInException();
         }
+
+        if (isFriend(user, loggedUser)) {
+            return getTripsBy(user);
+        }
+        return emptyList();
+
+
+    }
+
+    private boolean isFriend(User user, User loggedUser) {
+        boolean isFriend = false;
+        for (User friend : user.getFriends()) {
+            if (friend.equals(loggedUser)) {
+                isFriend = true;
+                break;
+            }
+        }
+        return isFriend;
     }
 
     protected List<Trip> getTripsBy(User user) {
